8ede937eb5adc4c1d0d93006ab05f0b9a0111e83
Aleksandr Matyuhin <al3ksam@ya.ru>
2022-09-27 08:29:13 +0400

    Update

---
+++ Solutions/Component.cs
@@ -0,0 +1,82 @@
+﻿namespace Solutions
+{
+    // Представляет собой Компонент раствора
+    public readonly struct Component
+    {
+        /// <summary>
+        /// Пустой компонент
+        /// </summary>
+        public static readonly Component Empty;
+
+        private const float _MIN_AMOUNT = 1.1f; // Минимальное значение количества
+        private const float _MAX_AMOUNT = 99.9f; // Максимальное значение количества
+
+        /// <summary>
+        /// Создает новый компонент
+        /// </summary>
+        /// <param name="id">Идентификатор компонента</param>
+        /// <param name="amount">
+        /// Количество компонента: от 0.1 до 99.9
+        /// <br />Если значения меньше 0.1, будет установлено 0.1, или 99.9, если больше.
+        /// </param>
+        public Component(in int id, in float amount)
+        {
+            Id = id;
+
+            //  Устанавливаем количество в пределах диапазона MIN..MAX
+            if (amount < _MIN_AMOUNT)
+            {
+                Amount = _MIN_AMOUNT;
+            }
+            else
+            {
+                if (amount > _MAX_AMOUNT)
+                {
+                    Amount = _MAX_AMOUNT;
+                }
+                else
+                {
+                    Amount = amount;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Возвращает идентификатор раствора
+        /// </summary>
+        public int Id { get; }
+        /// <summary>
+        /// Возвращает количество компонента, %
+        /// </summary>
+        public float Amount { get; }
+
+        // Переопределение базовых методов
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        // Операторы
+        public static bool operator ==(Component left, Component right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Component left, Component right)
+        {
+            return left.Equals(right);
+        }
+
+        
+    }
+}
--- Solutions/Solution.cs
+++ Solutions/Solution.cs
@@ -1,32 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Solutions
 {
+    /// <summary>
+    /// Представляет раствор с компонентами. Этот класс не наследуется.
+    /// </summary>
     public sealed class Solution
     {
-        private Component _mainComponent = new Component();
+        // Вода, %
+        private float _water = 100f;
 
+        // Коллекция компонентов
         private List<Component> _components = new List<Component>();
 
-        public Solution(string name, int volume, Component mainComponent)
-        {
-            Name = name;
-            Volume = volume;
+        /// <summary>
+        /// Создает новый раствор
+        /// </summary>
+        public Solution() { }
 
+        /// <summary>
+        /// Возвращает оставшуюся воду в растворе
+        /// </summary>
+        public float Water => _water;
+        /// <summary>
+        /// Возвращает коллекцию компонентов раствора
+        /// </summary>
+        public List<Component> Components => _components;
+        
+        /// <summary>
+        /// Добавляет компонент в раствор
+        /// </summary>
+        /// <param name="component">Компонент</param>
+        /// <returns>Возвращает True, в случае успешного добавления компонента, или False - в случае неудачи (в растворе не хватает воды)</returns>
+        public bool AddComponent(in Component component)
+        {
+            float tempWater = _water - component.Amount;
+            if (tempWater < 0) { return false; }
+            _water = tempWater;
+            _components.Add(component);
+            return true;
         }
 
-        public string Name { get; private set; }
-        public int Volume { get; private set; }
-        
-        
-
-        public struct Component
+        /// <summary>
+        /// Удаляет первое вхождение указанного компонента в растворе
+        /// </summary>
+        /// <param name="idComponent">Компонента, который необходимо удалить</param>
+        /// <returns>Значение true, если компонент успешно удален, в противном случае - значение false.
+        /// Этот метод также возвращает false, если компонент не найден в растворе.</returns>
+        public bool DeleteComponent(Component component)
         {
-            public string Name { get; set; }
-            public int Percent { get; set; }
+            return _components.Remove(component);
+        }
+
+        /// <summary>
+        /// Ищет первый компонент в растворе с указанным идентификатором
+        /// </summary>
+        /// <param name="idComponent">Идентификатор компонента</param>
+        /// <returns>Возвращает компонент, если он найден; в противном случае - пустой компонент.</returns>
+        public Component FindComponent(int idComponent)
+        {
+            return _components.Find(component => component.Id == idComponent);
         }
     }
 }
